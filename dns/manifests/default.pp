5a08fe92d035d05c9c1615ebf735ab86836984d8
jing-tw <mqjing@gmail.com>
2015-02-19 11:18:17 +0800

    use vagrant to manage the IP

--- dns/manifests/default.pp
+++ dns/manifests/default.pp
@@ -2,40 +2,9 @@
 # by Jing.
 
 node default {
-	  
-  # setup dns ip address
-   network_config { 'eth0':
-		ensure  => 'present',
-		family  => 'inet',
-		method  => 'dhcp',
-		onboot  => 'true',
-		hotplug => 'true',
-		options => {'pre-up' => 'sleep 2'},
-	}
-
-	network_config { 'lo':
-		ensure => 'present',
-		family => 'inet',
-		method => 'loopback',
-		onboot => 'true',
-	}
-
-	network_config { 'eth1':
-		ensure    => 'present',
-		family    => 'inet',
-		ipaddress => '192.168.50.2',
-		method    => 'static',
-		netmask   => '255.255.255.0',
-		onboot    => 'true',
-  }
-  
-  exec { "restart network":
-		command => "/sbin/ifdown eth1 && /sbin/ifup eth1",
-  }
-  
   # setup name
   file { "/etc/hostname": 		content => "server1"; 	}
-	exec { "setup hostname":		command => "/bin/hostname server1",	}
+  exec { "setup hostname":		command => "/bin/hostname server1",	}
   
   include dnsmasq
   # setup address mapping
--- vm3/manifests/default.pp
+++ vm3/manifests/default.pp
@@ -4,39 +4,9 @@
 # 	https://forge.puppetlabs.com/adrien/network
 
 node default {
-
-  network_config { 'eth0':
-		ensure  => 'present',
-		family  => 'inet',
-		method  => 'dhcp',
-		onboot  => 'true',
-		hotplug => 'true',
-		options => {'pre-up' => 'sleep 2'},
-	}
-
-	network_config { 'lo':
-		ensure => 'present',
-		family => 'inet',
-		method => 'loopback',
-		onboot => 'true',
-	}
-
-	network_config { 'eth1':
-		ensure    => 'present',
-		family    => 'inet',
-		ipaddress => '192.168.50.12',
-		method    => 'static',
-		netmask   => '255.255.255.0',
-		onboot    => 'true',
-  }
-  
-  exec { "restart network":
-		command => "/sbin/ifdown eth1 && /sbin/ifup eth1",
-  }
-  
   # setup name
   file { "/etc/hostname": 		content => "vm3"; 	}
-	exec { "setup hostname":		command => "/bin/hostname vm3",	}
+  exec { "setup hostname":		command => "/bin/hostname vm3",	}
   
   # setup customized DNS server
   file { "/etc/resolv.conf": content => "nameserver 192.168.50.2"; }
